b5a2b31c9cb42be3344f7dc91c9c148ba12c5bbc
Pedro M. Silva <pemiolsi@hotmail.com>
2019-03-29 19:37:17 +0000

    Add helpful .ToSql() method to queries

---
+++ foodss-backend/QueryExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+// Downloaded from
+// https://github.com/aspnet/EntityFrameworkCore/issues/6482#issuecomment-399061388
+namespace StorageSystem
+{
+    public static class IQueryableHelper
+    {
+        private static readonly FieldInfo _queryCompilerField = typeof(EntityQueryProvider).GetTypeInfo().DeclaredFields.Single(x => x.Name == "_queryCompiler");
+
+        private static readonly TypeInfo _queryCompilerTypeInfo = typeof(QueryCompiler).GetTypeInfo();
+
+        private static readonly FieldInfo _queryModelGeneratorField = _queryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_queryModelGenerator");
+
+        private static readonly FieldInfo _databaseField = _queryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_database");
+
+        private static readonly PropertyInfo _dependenciesProperty = typeof(Database).GetTypeInfo().DeclaredProperties.Single(x => x.Name == "Dependencies");
+
+        public static string ToSql<TEntity>(this IQueryable<TEntity> queryable)
+            where TEntity : class
+        {
+            if (!(queryable is EntityQueryable<TEntity>) && !(queryable is InternalDbSet<TEntity>))
+                throw new ArgumentException();
+
+            var queryCompiler = (IQueryCompiler)_queryCompilerField.GetValue(queryable.Provider);
+            var queryModelGenerator = (IQueryModelGenerator)_queryModelGeneratorField.GetValue(queryCompiler);
+            var queryModel = queryModelGenerator.ParseQuery(queryable.Expression);
+            var database = _databaseField.GetValue(queryCompiler);
+            var queryCompilationContextFactory = ((DatabaseDependencies)_dependenciesProperty.GetValue(database)).QueryCompilationContextFactory;
+            var queryCompilationContext = queryCompilationContextFactory.Create(false);
+            var modelVisitor = (RelationalQueryModelVisitor)queryCompilationContext.CreateQueryModelVisitor();
+            modelVisitor.CreateQueryExecutor<TEntity>(queryModel);
+            return modelVisitor.Queries.Join(Environment.NewLine + Environment.NewLine);
+        }
+    }
+}
