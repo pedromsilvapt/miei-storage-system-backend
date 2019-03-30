eeacacfce3148596127d25f6bd571b9b8e7ce2ff
Pedro M. Silva <pemiolsi@hotmail.com>
2019-03-30 10:25:12 +0000

    Add sample details method in storage controller

--- foodss-backend/Controllers/StorageController.cs
+++ foodss-backend/Controllers/StorageController.cs
@@ -8,6 +8,16 @@ using StorageSystem.Models;
 
 namespace StorageSystem.Controllers
 {
+    public class StorageDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+        public StorageType Type { get; set; }
+
+        public static StorageDTO FromModel(Storage model) => new StorageDTO() { Id = model.Id, Name = model.Name, OwnerId = model.OwnerId, Type = model.Type };
+    }
+
     [Route("api/[controller]")]
     [ApiController]
     public class StorageController : Controller
@@ -20,7 +30,7 @@ namespace StorageSystem.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Object>>> GetStorages()
+        public async Task<IEnumerable<StorageDTO>> GetStorages()
         {
             // Ideally this should be retrieved from some session variable to identify the user who made the request
             var userId = 1;
@@ -33,9 +43,32 @@ namespace StorageSystem.Controllers
 
             return await context.Storages
                 .Where(storage => storage.OwnerId == userId || storageIds.Contains(storage.Id))
-                .Select(storage => new { storage.Id, storage.Name, storage.OwnerId, storage.Type })
+                .Select(storage => StorageDTO.FromModel(storage))
                 .ToListAsync();
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StorageDTO>> FindStorageById(int id)
+        {
+            var userId = 1;
+
+
+            Storage storage = await context.Storages
+                .Include(s => s.Users)
+                .FirstOrDefaultAsync(s => s.Id == id);
+
+            if (storage == null)
+            {
+                return NotFound();
+            }
+
+            // If the user is neither the owner of the storage, nor is he inside the user's collection, we return an error
+            if ((storage.OwnerId != userId) && (storage.Users.FirstOrDefault(u => u.UserId == userId) == null))
+            {
+                return this.Unauthorized();
+            }
+
+            return StorageDTO.FromModel(storage);
+        }
     }
 }
