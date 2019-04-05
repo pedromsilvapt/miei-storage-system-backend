082344fdeae9ee050fb2169d95e9742e50972b33
Pedro M. Silva <pemiolsi@hotmail.com>
2019-04-05 20:56:20 +0100

    Update migrations (automatic)

--- foodss-backend/Migrations/20190405195401_CreateProductDates.Designer.cs
+++ foodss-backend/Migrations/20190405195401_CreateProductDates.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StorageSystem.Models;
 
 namespace StorageSystem.Migrations
 {
     [DbContext(typeof(StorageSystemContext))]
-    partial class StorageSystemContextModelSnapshot : ModelSnapshot
+    [Migration("20190405195401_CreateProductDates")]
+    partial class CreateProductDates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ foodss-backend/Migrations/20190405195401_CreateProductDates.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StorageSystem.Migrations
+{
+    public partial class CreateProductDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvailableCount",
+                table: "ProductItems");
+
+            migrationBuilder.DropColumn(
+                name: "ConsumedCount",
+                table: "ProductItems");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiryDate",
+                table: "ProductItems",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AddedDate",
+                table: "ProductItems",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ConsumedDate",
+                table: "ProductItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddedDate",
+                table: "ProductItems");
+
+            migrationBuilder.DropColumn(
+                name: "ConsumedDate",
+                table: "ProductItems");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiryDate",
+                table: "ProductItems",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AvailableCount",
+                table: "ProductItems",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ConsumedCount",
+                table: "ProductItems",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
