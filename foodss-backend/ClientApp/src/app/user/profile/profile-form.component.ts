59ce86df0099b05dd260b9e93ec5664fdfb4f1d9
Pedro M. Silva <pemiolsi@hotmail.com>
2019-06-28 18:41:17 +0100

    Add toggle to change password

--- foodss-backend/ClientApp/src/app/user/profile/profile-form.component.ts
+++ foodss-backend/ClientApp/src/app/user/profile/profile-form.component.ts
@@ -1,10 +1,6 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {User} from '../model/user.model';
-import {StepperComponent} from '../components/stepper/stepper.component';
-import {Language} from 'angular-l10n';
-import {UserRegisterDTO} from '../model/user-register.dto.model';
-import {UserService} from '../user.service';
-import {MessageUtil} from '../../shared/util/message.util';
+import { Component, OnInit } from '@angular/core';
+import { Language } from 'angular-l10n';
+import { MessageUtil } from '../../shared/util/message.util';
 import { HttpService } from 'src/app/core/http/http.service';
 
 interface User {
@@ -23,29 +19,37 @@ export class ProfileFormComponent implements OnInit {
 
   @Language() lang: string;
 
+  public changePassword: boolean = false;
+
   public user: User;
-  
-  constructor(private http : HttpService, private messageUtil: MessageUtil) { }
+
+  constructor(private http: HttpService, private messageUtil: MessageUtil) { }
 
   async ngOnInit() {
     this.user = await this.http.get('user/session').toPromise();
   }
 
   async save() {
-    if (this.user.password == this.user.passwordConfirm) {
-      try {
-        await this.http.post('user', {
-          name: this.user.name,
-          password: this.user.password,
-          passwordConfirm: this.user.passwordConfirm,
-        }).toPromise();
+    if (this.changePassword && this.user.password == this.user.passwordConfirm) {
+      return;
+    }
 
-        this.messageUtil.addSuccessMessage("Perfil", "Perfil alterado.");
-      } catch (err) {
-        console.error(err);
+    if (this.changePassword && !this.user.password) {
+      return;
+    }
 
-        this.messageUtil.addErrorMessage("Perfil", "Erro ao alterar perfil.")
-      }
+    try {
+      await this.http.post('user', {
+        name: this.user.name,
+        password: this.changePassword ? this.user.password : null,
+        passwordConfirm: this.changePassword ? this.user.passwordConfirm : null,
+      }).toPromise();
+
+      this.messageUtil.addSuccessMessage("Perfil", "Perfil alterado.");
+    } catch (err) {
+      console.error(err);
+
+      this.messageUtil.addErrorMessage("Perfil", "Erro ao alterar perfil.");
     }
   }
 }
