c5efb1e26c434529bf0b6302c2d680a7b7bff638
Diogo Fernandes <a78867@alunos.uminho.pt>
2019-07-01 13:53:07 +0100

    Removing unnecessary folders

--- deployAPP/benchmark/generate_data.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-file = open(sys.argv[1], 'w')
-file.write('name,username,email,password,project\n')
-for i in range(int(sys.argv[2]),int(sys.argv[3])+1):
-    file.write('name' + str(i) + ',username' + str(i) + ',email' + str(i) + '@email.com,password' + str(i) + ',project' + str(i) + '\n')
-file.close()
--- deployAPP/db-install/files/start_bucardo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo mkdir -p /var/run/bucardo
-sleep 10
-sudo bucardo start
